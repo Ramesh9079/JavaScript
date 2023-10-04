d54726c5c688c982717b6f9a4a203c691766e96d
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-10-04 11:40:47 +0000

    DataTypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // We are using nodejs , not browser
+
+console.log(3+3)
+console.log("ramesh");
+
+let name = "Ramesh";
+let age = 22;
+let isLoggedIn = true;
+
+// Number ==> 2 to power 53
+// bigint 
+// string ==> "" or ''
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==>  not defined value for the variable
+// symbol ==> unique
+
+// object => 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // objects
