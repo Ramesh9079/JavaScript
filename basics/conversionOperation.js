76e26ccee1dd6b590997904dc8aa06cd363b1636
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-10-06 03:02:46 +0000

    Conversion of data type

---
+++ basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "43abc";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==>1, false ==> 0
+
+
+let isLogged = ""
+
+let booleanISLogged = Boolean(isLogged);
+console.log(booleanISLogged)
+
+// 1 ==> true and 0 ==> false
+// "" ==> false
+// "Ramesh" ==> true
+
+let number = 33
+
+let stringNumber = String(number);
+console.log(stringNumber)
+console.log( typeof stringNumber);
