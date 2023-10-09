cef72f9190ceda5117d0717aff7f969ffb5e83b2
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-10-09 03:40:46 +0000

    Comparison of data types

---
+++ basics/comparison.js
@@ -0,0 +1,12 @@
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// Comparison and equality works differently. 
+
+console.log("2" === 2);
