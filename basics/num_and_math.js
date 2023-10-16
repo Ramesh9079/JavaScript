0179e3c451aa7509ced64afbbe3ba04451e6e4c8
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-10-16 05:19:19 +0000

    Numbers and Maths

---
+++ basics/num_and_math.js
@@ -0,0 +1,35 @@
+
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); // It will put commas on the number for readable of the value.
+
+
+// ++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++ 
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // Absolute value
+// console.log(Math.round(3.5)); // Round off
+// console.log(Math.ceil(6.3)); // It will round off at highest value (eg: 5.2 => 6)
+// console.log(Math.floor(8.9)); // It will round off at lowest value (eg: 8.9 => 8)
+
+// Math.min => minimum value at the array
+// Math.max => maximum value at the array
+
+
+console.log(Math.floor(Math.random()*10) +1);
+
+
+
+
