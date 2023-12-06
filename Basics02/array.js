a92e98030974bd053ecab1cbc2e5a9d0949b4e74
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-06 02:21:46 +0000

    Array

---
+++ Basics02/array.js
@@ -0,0 +1,35 @@
+// Array
+
+const myArr =[0,1,2,3,4,5]
+const myHeores = ["Ironman","Spiderman"];
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[0]);
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+const months = ['Jan','Feb','Mar','April','May']
+const mn1 = months.slice(1,3)
+console.log(mn1);
+console.log("A",months);
+
+const mn2 = months.splice(1,3)
+console.log(mn2);
+console.log("B",months);
+
+
+// console.log("A",myArr);
+// const nm1 = myArr.slice(1,3);
+// console.log(nm1);
+// console.log("B",myArr);
+// const nm2 = myArr.splice(1,3)
+// console.log(nm2);
+// console.log("C",myArr);
