115a19ec028ecd376acd95b4042fc2388af0c3b4
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-11 05:55:34 +0000

    Objects in intermediate

---
+++ Basics02/object2.js
@@ -0,0 +1,54 @@
+
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+        usersName: {
+            firstname :"Ramesh",
+            lastname:"Tamang"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.usersName.firstname);
+
+const obj1 = { 1:'a',2:'b'
+}
+const obj2 = {3:'a', 4:'b'}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1, obj2);
+
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+
+const users = [
+    {
+        id :1,
+        email:'ramesh@facebook.com'
+    }
+,
+    {
+
+    }
+    
+]
+
+// console.log(users[0].email);
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser))
+console.log(tinderUser.hasOwnProperty('username'))
