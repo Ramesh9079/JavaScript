60b1411bf9f8e063a69cc2283ee5ee69075972c1
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-11 11:07:20 +0000

    function concept

---
+++ basics3/function.js
@@ -0,0 +1,35 @@
+
+
+function sayMyName (){
+    console.log("R");
+    console.log("A"); 
+    console.log("M");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+// sayMyName()
+
+// function addTwoNumbers (number1, number2){
+// console.log(number1 + number2);
+// }
+
+function addTwoNumbers (number1, number2){
+// let result = number1 + number2
+// return result
+return number1+ number2
+
+}
+const result = addTwoNumbers(3,4)
+// console.log(result);
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage('Ramesh'))
+
+console.log(loginUserMessage())
+
+// If you don not pass the argument in the function then it will give undefined. 
+
