53191bc8901629b1fe1063c8c25a64b2f87e9b27
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-12 06:21:43 +0000

    Scope

---
+++ basics3/scope.js
@@ -0,0 +1,20 @@
+
+// if (true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+    
+// }
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+
+let carMarkedPrice = 14000;
+
+if (true){
+    let carMarkedPrice = 12000
+    carSellingPrice = 10000
+    console.log(`Car Price is :`, carMarkedPrice);
+}
+
+console.log(carMarkedPrice);
