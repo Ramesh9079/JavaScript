43afea412a456b06bad3256407855a6a2d40dcb2
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-12 22:25:30 +0000

    Scope in depth

--- basics3/scope.js
+++ basics3/scope.js
@@ -1,20 +1,58 @@
 
-// if (true){
-//     let a = 10
-//     const b = 20
-//     var c = 30
+// // if (true){
+// //     let a = 10
+// //     const b = 20
+// //     var c = 30
     
-// }
-// // console.log(a);
-// // console.log(b);
-// console.log(c);
+// // }
+// // // console.log(a);
+// // // console.log(b);
+// // console.log(c);
 
-let carMarkedPrice = 14000;
+// let carMarkedPrice = 14000;
+
+// if (true){
+//     let carMarkedPrice = 12000
+//     carSellingPrice = 10000
+//     console.log(`Car Price is :`, carMarkedPrice);
+// }
+
+// console.log(carMarkedPrice);
+
+
+function one (){
+    const username = "Ramesh"
+
+    function two (){
+        const website = 'google'
+        // console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+one()
 
 if (true){
-    let carMarkedPrice = 12000
-    carSellingPrice = 10000
-    console.log(`Car Price is :`, carMarkedPrice);
+    const username = "Ramesh"
+    if (username === "Ramesh"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+//   Interesting Question
+
+console.log(addone(5))
+function addone (num){
+return num+1
 }
 
-console.log(carMarkedPrice);
+//  We can't execute the expression function before declaring the expression. 
+
+const addTwo = function(num){
+    return num +2
+}
+console.log(addTwo(5))
+
