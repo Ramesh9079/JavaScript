3d237501914c428431b684d9ae5937ba2df624ea
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-19 22:40:54 +0000

    for loop types

---
+++ Loop/two.js
@@ -0,0 +1,47 @@
+//  for of loop 
+
+// const number = [1,2,3,4,5]
+
+// for (dataset of number){
+//     console.log(dataset)
+// }
+
+// const myObject = {
+//     name: 'Ramesh Tamang',
+//     qualification: 'Bachelor Degree',
+//     designation:'Future Software Developer'
+// }
+
+// for (dream in myObject){
+//     console.log(dream+ ":" + myObject[dream])
+// }
+
+
+
+//  For Each Loop
+
+// const myObject = [{
+//     name: 'Ramesh Tamang',
+//     qualification: 'Bachelor Degree',
+//     designation:'Future Software Developer'
+// }]
+
+// myObject.forEach( (item)=> {
+//     console.log(item.qualification)
+// })
+
+//  For Each loop by declaring the function before executing it. 
+
+// const parameters =["name", "address","email", "contact"]
+
+// function introduction (detail){
+//     console.log(detail)
+// }
+// parameters.forEach(introduction)
+
+
+// const parameters =["name", "address","email", "contact"]
+// parameters.forEach((item, index, arr)=>{
+//     console.log(item, index, arr)
+// })
+
