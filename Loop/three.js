cf36b9809e1b78276b93e47123c40f86996a4098
Ramesh9079 <119543350+Ramesh9079@users.noreply.github.com>
2023-12-19 22:50:10 +0000

    for each loop (array=>object)

---
+++ Loop/three.js
@@ -0,0 +1,23 @@
+
+const detail = [ 
+    {
+        name:"Ramesh Tamang",
+        education: "Information technology"
+    },
+    {
+        name:"Sujan Tamang",
+        education: "Information technology"
+    },
+    {
+        name:"Aakriti Sharma",
+        education: "Nursing"
+    },
+    {
+        name:"Krishna Neupane",
+        education: "Automobile Engineering"
+    }
+]
+
+detail.forEach((item)=>{
+    console.log(item.education)
+})
